14af6aaa3bcfd07c75a2d46dbb0cc57fc0cc9af5
myokoyama <part7steelballrun.to.8@gmail.com>
2025-12-10 23:07:20 +0900

    feat: ✨ update settings a little

--- nvim/lua/myconfig.lua
+++ nvim/lua/myconfig.lua
@@ -298,10 +298,10 @@ require('hlslens').setup({
 local kopts = {noremap = true, silent = true}
 
 vim.api.nvim_set_keymap('n', 'n',
-    [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
+    [[<Cmd>execute('keepjumps normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
     kopts)
 vim.api.nvim_set_keymap('n', 'N',
-    [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
+    [[<Cmd>execute('keepjumps normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
     kopts)
 
 -- vim-asterisk integration
